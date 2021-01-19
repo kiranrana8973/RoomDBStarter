f787b52c950e648f74990806babdfe2e6a84981e
Kiran Rana <kiranrana8973@gmail.com>
2021-01-19 21:44:51 +0545

    Initial commit

---
+++ app/src/main/java/com/kiran/test/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.kiran.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        
+    }
+}
